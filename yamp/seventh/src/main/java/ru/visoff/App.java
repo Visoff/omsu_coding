8eb9cb13fa0229bec723ee709aebfdbd402fc601
Ilya <ikalinin01@mail.ru>
2026-03-23 15:23:58 +0600

    wip(yamp): seventh

---
+++ yamp/seventh/src/main/java/ru/visoff/App.java
@@ -0,0 +1,16 @@
+package ru.visoff;
+
+import java.io.InputStream;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        InputStream in;
+        System.out.println( "Hello World!" );
+    }
+}
